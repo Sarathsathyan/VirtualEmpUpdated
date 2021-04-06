713665cb5854d9f2cc3684cbb244c8abd94af91a
RahulAgarwal31101999 <rahul.agarwal31101999@gmail.com>
2021-04-06 17:55:11 +0530

    user blog page

--- User/urls.py
+++ User/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('userProfileEdit/', views.userProfileEdit, name='userprofileedit'),
     path('userCFP/', views.userCfp, name="usercfp"),
     path('userQuizz/<int:w_id>/', views.userQuizz, name="userquizz"),
+    path('user_blog_page/',views.user_blog_page,name='user_blog_page'),
+    path('userblogsdetail/<int:id>',views.userblogsdetail,name='userblogsdetail'),
+
 
     path('userResult/', views.userResult, name="userResult"),
     path('userProject/', views.userProjects, name="userprojects"),
@@ -19,6 +22,7 @@ urlpatterns = [
     path('pricing/', views.pricing, name="pricing"),
     path('license_generate/',views.license_generate,name='license_generate'),
     path('license_page/',views.license_page,name='license_page'),
+
 
 
     path('major_project_dashboard/',views.major_project_dashboard,name='major_project_dashboard'),
